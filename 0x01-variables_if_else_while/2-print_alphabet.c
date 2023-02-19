fe37405f226e4f767109ed492f30a20e870b490c
Tesfa56 <tes>
2023-02-19 10:49:12 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets a-z
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putcar("\n");
+	return (0);
+}
