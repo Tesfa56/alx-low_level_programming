267b256e39d38091827566b147a08897e9f58ce8
Tesfa56 <tes>
2023-02-19 11:42:40 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets a-z except e and q
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
