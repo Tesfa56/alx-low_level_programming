206bcccd9ca6711a283d64c46a6286ac2f62e7eb
Tesfa56 <tes>
2023-02-19 12:51:21 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print 0123456789abcdef
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char ch[16] = "0123456789abcdef";
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(ch[i]);
+	}
+	putchr('\n');
+	return (0);
+}
