9cb5868d053a50e47dd7c682f92f324cbf32a789
Tesfa56 <tes>
2023-02-20 10:34:18 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers with comma separeted
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i % 10) + '0');
+		for (i = 1; i < 10; i++)
+		{
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
