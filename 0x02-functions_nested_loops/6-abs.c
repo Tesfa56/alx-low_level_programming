358ac9eb1016b7167a29a4f5a5457b464f441003
Tesfa56 <tes>
2023-02-21 11:33:37 -0800

    asolute vlaue

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - return absluute vlaue of a number
+ *
+ * @n: any number
+ *
+ * Return: number
+ *
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
