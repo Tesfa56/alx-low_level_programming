fcf79f44b34f21bedcb8b152483221cd929e8964
Tesfa56 <tes>
2023-02-21 10:11:44 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet a-z
+ *
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch =  'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
+
