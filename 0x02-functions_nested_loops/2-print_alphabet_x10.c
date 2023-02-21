927560fde80463fd993ff8e21b90f19713887429
Tesfa56 <tes>
2023-02-21 10:27:58 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet a-z ten times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
